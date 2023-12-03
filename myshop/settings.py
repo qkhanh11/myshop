de4251e464b303a6a820eb08dd82d182744a4326
qkhanh11 <khanh1320306@gmail.com>
2023-12-03 09:56:32 +0700

    connect db

--- myshop/settings.py
+++ myshop/settings.py
@@ -76,9 +76,11 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "read_default_file": "./db.cnf",
+        },
     }
 }
 

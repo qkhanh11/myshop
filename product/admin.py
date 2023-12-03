c4476c4816d96e30fe7faabd7a24cd0a1003df61
qkhanh11 <khanh1320306@gmail.com>
2023-12-03 10:14:25 +0700

    create product

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django import forms
+from .models import ProductModel
+
+class ProductAdminForm(forms.ModelForm):
+    class Meta:
+        model = ProductModel
+        fields = '__all__'
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["product_name", "price", "quantity", "create_at"]
+    list_filter = ["create_at","category"]
+    search_fields = ["product_name"]
+    form = ProductAdminForm 
+    
+
+admin.site.register(ProductModel,ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from category.models import CategoryModel
+import os
+
 
 # Create your models here.
+class ProductModel(models.Model):
+    product_name = models.CharField(max_length=100)
+    summary = models.CharField(max_length=500,null = True)
+    price = models.IntegerField(null=True)
+    quantity = models.IntegerField(default=0)
+    description = models.TextField(null=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    delete_flag = models.BooleanField(default=False)
+    image = models.ImageField(null=True,upload_to="images/")
+    category = models.ForeignKey(CategoryModel,on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.product_name
+    
